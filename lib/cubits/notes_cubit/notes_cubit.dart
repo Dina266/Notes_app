e58e608abd4db2fc36cab68ceb601b2e4767cd13
Dina Ahmed <denaahmed340@gmail.com>
2023-12-03 14:17:44 +0200

    fetch note

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -10,13 +10,13 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+
+  List <NoteModel>? notes ;
   fetchAllNotes () {
-    try {
+    
       var notesBox = Hive.box<NoteModel>(kNotesBox);
-      var notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(errMessage: e.toString()));
-    }
+      notes = notesBox.values.toList();
+    
+    
   }
 }
--- lib/widgets/note_item_builder.dart
+++ lib/widgets/note_item_builder.dart
@@ -1,20 +1,32 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/edit_notes.dart';
 
+import '../models/note_model.dart';
 import 'notes_item.dart';
 
 class NoteItemBuilder extends StatelessWidget {
   const NoteItemBuilder({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder:(context , index)=> Padding(padding: const EdgeInsets.symmetric(vertical: 4),
-      child: GestureDetector(
-        onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => const EditNote(),)),
-        child: const NoteItem())
-      ,)
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List <NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes?? [];
+        return ListView.builder(
+          itemCount: notes.length,
+            itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const EditNote(),
+                          )),
+                      child: const NoteItem()),
+                ));
+      },
     );
   }
 }
